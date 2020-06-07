8f15d2252a94979ffc23ddf2e85a2ab98b19f9d1
Rajeshroulo <rajraval017@gmail.com>
2020-06-07 18:32:39 +0530

    Added code for comparison among different weight quantities

--- src/main/java/quantitymeasurement/Unit.java
+++ src/main/java/quantitymeasurement/Unit.java
@@ -5,7 +5,10 @@ public enum Unit {
     FEET(12.0,"LENGTH"), INCH(1.0,"LENGTH"),
     YARD(36.0,"LENGTH"), CENTIMETER(0.4,"LENGTH"),
     LITRE(1.0,"VOLUME"), GALLON(3.78,"VOLUME"),
-    MILLILITRE(1.0/1000.0,"VOLUME");
+    MILLILITRE(1.0/1000.0,"VOLUME"),GRAM(1.0,"WEIGHT"),
+    KILOGRAM(1000.0,"WEIGHT"),
+    TONNE(1000000.0,"WEIGHT");
+
     private final Double conversionValue;
     final String measurementType;
 
